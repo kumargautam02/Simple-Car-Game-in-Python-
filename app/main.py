a29ecc0a4de3ffd9a302f629197dc812d28d3b0d
gautam <gautam.kuamr410305@gmail.com>
2024-09-03 21:51:15 +0530

    i am updating

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ height =500
 
 screen_size = (width, height)
 screen=  pygame.display.set_mode(screen_size)
-pygame.display_set_caption("car Game")
+pygame.display.set_caption("car Game")
 
 
 ## colors
@@ -31,6 +31,31 @@ clock = pygame.time.Clock()
 fps = 120
 running =True
 
+# markers size
+marker_width = 10
+marker_height = 50
+
+#road and edge markers
+road = (100, 0, 300, height)
+left_edge_marker = (95, 0, marker_width, height)
+right_edge_marker = (395, 0, marker_width, height)
+
+#coordinates of lanes
+left_lane = 150
+centre_lane = 250
+
+right_lane= 350
+lanes = [left_lane, centre_lane, right_lane]
+
+# for animating movement of the lane markers
+lane_marker_move_y = 0
+
+
+
+
+
+
+#gane loop
 while running:
 
     clock.tick(fps)
@@ -38,6 +63,25 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
+
+
+    #draw grass
+    screen.fill(green)
+
+    #draw the road
+    pygame.draw.rect(screen, grey, road)
+    #draw the edge markers
+    pygame.draw.rect(screen, yellow, left_edge_marker)
+    pygame.draw.rect(screen, yellow, right_edge_marker)
+
+    lane_marker_move_y += 2
+    if lane_marker_move_y >= marker_height *2:
+        lane_marker_move_y = 0
+
+    #draw the lane marker
+    for y in range(marker_height * -2, height, marker_height*2):
+        pygame.draw.rect(screen, white, (left_lane +45, y+lane_marker_move_y, marker_width, marker_height))
+        pygame.draw.rect(screen, white, (centre_lane +45, y+lane_marker_move_y, marker_width, marker_height))
     pygame.display.update()
 
 pygame.quit()
